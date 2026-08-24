bcc86ba0049b066761eba08990b020deb3fec2a3
Kytolly <kytolly.xqy@outlook.com>
2026-08-24 20:22:32 +0800

    can be installed

--- src/git.ts
+++ src/git.ts
@@ -6,8 +6,10 @@ import type { CommittedArtifact, GitAuthConfig, GitStatus, MemoryRecord, MemoryR
 import { renderSkillDraft, sanitizeSegment, slugify } from './strategy.js'
 
 export class GitEvolutionError extends Error {
-  constructor(message: string, public readonly code: string, cause?: unknown) {
+  code: string
+  constructor(message: string, code: string, cause?: unknown) {
     super(message)
+    this.code = code
     this.name = 'GitEvolutionError'
     if (cause !== undefined) {
       this.cause = cause
